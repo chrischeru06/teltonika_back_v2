cf59f81627a06faecc50dd98f082a1cee53eb996
DerickBdi <dkazimushahra@gmail.com>
2025-05-20 14:57:09 +0200

    amelioration

--- constants/IMAGES_DESTINATIONS.js
+++ constants/IMAGES_DESTINATIONS.js
@@ -9,11 +9,11 @@ const IMAGES_DESTINATIONS = {
           publicite:path.sep +'uploads' + path.sep + 'images' + path.sep + 'publicite',
           VehiculeMarque:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'vehiculeMarques',
           VehiculeModele:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'vehiculeModeles',
-          Proprietairepassport:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'Proprietairepassport',
-          Proprietairelogo:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'Proprietairelogo',
-          Proprietairefilecni:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'Proprietairefilecni',
-          Proprietairefilenif:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'Proprietairefilenif',
-          Proprietairefilerc:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'Proprietairefilerc',
+          Proprietairepassport:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'proprietairepassport',
+          Proprietairelogo:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'proprietairelogo',
+          Proprietairefilecni:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'proprietairefilecni',
+          Proprietairefilenif:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'proprietairefilenif',
+          Proprietairefilerc:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'proprietairefilerc',
           assureur:path.sep + 'uploads' + path.sep + 'images' + path.sep + 'assureur',
 }
 
